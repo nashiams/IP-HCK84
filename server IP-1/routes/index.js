21f5744c6a67d0a973ce898fe8f3a1e7c10d9059
nashiams <ihsanmuhammadn92@gmail.com>
2025-06-25 00:28:08 +0700

    chore: class upload

--- server IP-1/routes/index.js
+++ server IP-1/routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const authRoutes = require("./authRoutes");
 const requirementsRoutes = require("./requirementsRoutes");
 const uploadRoutes = require("./uploadRoutes");
-// const analyzeRoutes = require("./routes/analyzeRoutes");
+// const analyzeRoutes = require("./analyzeRoutes");
 
 router.use("/api/auth", authRoutes);
 
 // Protected routes (require Bearer token)
 router.use("/api/requirements", requirementsRoutes);
 router.use("/api/upload", uploadRoutes);
-// app.use("/api/analyze", authenticate, analyzeRoutes);
+// router.use("/api/analyze", analyzeRoutes);
 module.exports = router;
--- server IP-1/routes/uploadRoutes.js
+++ server IP-1/routes/uploadRoutes.js
@@ -3,5 +3,8 @@ const UploadController = require("../controllers/uploadController");
 const router = express.Router();
 
 router.post("/", UploadController.upload);
+router.get("/tes", (req, res) => {
+  res.send("tes");
+});
 
 module.exports = router;
